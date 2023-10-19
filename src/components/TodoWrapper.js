90119a16cf72c1ad0904eb3cffd3119a75a4179b
gsampaiowz <gabrielsampaio1216@gmail.com>
2023-10-19 02:10:03 -0300

    Local storage estudando

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -9,120 +9,121 @@ import FadeIn from "react-fade-in";
 uuidv4();
 
 export const TodoWrapper = () => {
-	const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-	const addTodo = (todo) => {
-		setTodos([
-			...todos,
-			{ id: uuidv4(), task: todo, completed: false, idEditing: false },
-		]);
-	};
+  const addTodo = (todo) => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, idEditing: false },
+    ]);
+  };
 
-	const toggleComplete = (id) => {
-		setTodos(
-			todos.map((todo) =>
-				todo.id === id ? { ...todo, completed: !todo.completed } : todo
-			)
-		);
-	};
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
 
-	const deleteTodo = (id) => {
-		setTodos(todos.filter((todo) => todo.id !== id));
-	};
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-	const editTodo = (id) => {
-		setTodos(
-			todos.map((todo) =>
-				todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
-			)
-		);
-	};
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
 
-	const editTask = (task, id) => {
-		setTodos(
-			todos.map((todo) =>
-				todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
-			)
-		);
-	};
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
 
-	// Pega a largura atualizada da tela para aprimorar responsividade
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  // Pega a largura atualizada da tela para aprimorar responsividade
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-	React.useEffect(() => {
-		const handleResize = () => {
-			setWindowWidth(window.innerWidth);
-		};
+  React.useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
 
-		window.addEventListener("resize", handleResize);
+    window.addEventListener("resize", handleResize);
 
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-	// Adiciona borda quando aparece a barra de rolagem
-	var lista = document.querySelector(".lista");
-	var todosFlex = document.querySelector(".todosFlex");
+  // Adiciona borda quando aparece a barra de rolagem
+  var lista = document.querySelector(".lista");
+  var todosFlex = document.querySelector(".todosFlex");
 
-	// CSS função
-	const addExceededHeightClass = () => {
-		if (lista) {
-			const listabox = document.querySelector(".lista-box")
-			if (todos.map((todo) => todo).length > 3) {
-				listabox.style.borderBottom = "1px solid rgba(255,255,255,0.5)";
-				listabox.style.borderTop = "1px solid rgba(255,255,255,0.5)";
-				todosFlex.style.padding = "16px 8px";
-			} else {
-				listabox.style.borderTop = "none";
-				listabox.style.borderBottom = "none";
-				todosFlex.style.padding = "4px 8px";
+  // CSS função
+  const addExceededHeightClass = () => {
+    if (lista) {
+      const listabox = document.querySelector(".lista-box");
+      if (todos.map((todo) => todo).length > 3) {
+        listabox.style.borderBottom = "1px solid rgba(255,255,255,0.5)";
+        listabox.style.borderTop = "1px solid rgba(255,255,255,0.5)";
+        todosFlex.style.padding = "16px 8px";
+      } else {
+        listabox.style.borderTop = "none";
+        listabox.style.borderBottom = "none";
+        todosFlex.style.padding = "4px 8px";
+      }
+    }
+  };
 
-			}
-		}
-	}
+  // evento da lista
+  if (lista)
+    lista.addEventListener("DOMSubtreeModified", addExceededHeightClass);
 
-	// evento da lista
-	if(lista)lista.addEventListener("DOMSubtreeModified", addExceededHeightClass);
+  // evento do flex
+  if (todosFlex) todosFlex.addEventListener("resize", addExceededHeightClass);
 
-	// evento do flex
-	if(todosFlex)todosFlex.addEventListener("resize", addExceededHeightClass);
-
-	return (
-		<div className="TodoWrapper">
-			<h1>Organize suas tarefas!</h1>
-			<TodoForm windowWidth={windowWidth} addTodo={addTodo} />
-			<Box className="lista-box" maxWidth={400} width={"100%"}>
-				<List disablePadding id={1} className="lista">
-					<TransitionGroup className="todosFlex">
-						{todos.map((todo) =>
-							todo.isEditing ? (
-								<Collapse key={todo.id}>
-									<FadeIn>
-										<EditTodoForm
-											windowWidth={windowWidth}
-											{...todo}
-											editTodo={editTask}
-											todo={todo}
-										/>
-									</FadeIn>
-								</Collapse>
-							) : (
-								<Collapse key={todo.id}>
-									<FadeIn>
-										<Todo
-											todo={todo}
-											toggleComplete={toggleComplete}
-											deleteTodo={deleteTodo}
-											editTodo={editTodo}
-										/>
-									</FadeIn>
-								</Collapse>
-							)
-						)}
-					</TransitionGroup>
-				</List>
-			</Box>
-		</div>
-	);
+  return (
+    <div className="TodoWrapper">
+      <h1>Organize suas tarefas!</h1>
+      <TodoForm windowWidth={windowWidth} addTodo={addTodo} />
+      <Box className="lista-box" maxWidth={400} width={"100%"}>
+        <List disablePadding id={1} className="lista">
+          <TransitionGroup className="todosFlex">
+            {todos.map((todo) =>
+              todo.isEditing ? (
+                <Collapse key={todo.id}>
+                  <FadeIn>
+                    <EditTodoForm
+                      windowWidth={windowWidth}
+                      {...todo}
+                      editTodo={editTask}
+                      todo={todo}
+                    />
+                  </FadeIn>
+                </Collapse>
+              ) : (
+                <Collapse key={todo.id}>
+                  <FadeIn>
+                    <Todo
+                      todo={todo}
+                      toggleComplete={toggleComplete}
+                      deleteTodo={deleteTodo}
+                      editTodo={editTodo}
+                    />
+                  </FadeIn>
+                </Collapse>
+              )
+            )}
+          </TransitionGroup>
+        </List>
+      </Box>
+    </div>
+  );
 };
